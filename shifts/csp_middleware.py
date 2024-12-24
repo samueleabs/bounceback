5d5fc7f39cb1f3af11c2942cf7f67760b8b680b9
samueleabs <samuel.eabs@gmail.com>
2024-12-24 23:47:43 +0000

    changed middlewarwe

--- shifts/csp_middleware.py
+++ shifts/csp_middleware.py
@@ -7,7 +7,7 @@ class ContentSecurityPolicyMiddleware:
         response = self.get_response(request)
         response['Content-Security-Policy'] = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; "
             "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
             "img-src 'self' data:; "
             "font-src 'self' https://cdn.jsdelivr.net; "
